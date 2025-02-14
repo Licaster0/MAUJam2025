14865073846e3cc2327bd0c76c5c0586d8116041
Licaster0 <ahmet0434recep@gmail.com>
2025-02-15 01:09:42 +0300

    31

--- MauJam2025/Assets/Scripts/Doors.cs
+++ MauJam2025/Assets/Scripts/Doors.cs
@@ -8,13 +8,36 @@ public class Doors : MonoBehaviour
     [Header("Sayilar")]
     public int doorCount;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    [Header("Referances")]
+    public GameObject DoorUiPanel;
+
+    private void Awake()
     {
-        if (collision.CompareTag("Player"))
+        DoorUiPanel = GameObject.Find("DoorUiPanel").GetComponent<GameObject>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
         {
             GameManager.instance.playerLightCount -= doorCount;
-            gameObject.SetActive(false);
+            DoorUiPanel.SetActive(true); // Uyarýyý aç
         }
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            DoorUiPanel.SetActive(false); // Oyuncu uzaklaþýnca kapat
+        }
+    }
+    public void DoorOpen()
+    {
+        DoorUiPanel.SetActive(false); // UI paneli kapat
+        gameObject.SetActive(false);
+    }
 
+    public void Cancel()
+    {
+        DoorUiPanel.SetActive(false); // UI paneli kapat
+    }
 }
