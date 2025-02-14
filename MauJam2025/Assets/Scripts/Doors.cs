f968402b409e7c28f6b51b294a87808c1f0a48cc
Licaster0 <ahmet0434recep@gmail.com>
2025-02-15 00:44:10 +0300

    1234

--- MauJam2025/Assets/Scripts/Doors.cs
+++ MauJam2025/Assets/Scripts/Doors.cs
@@ -7,13 +7,13 @@ public class Doors : MonoBehaviour
 
     [Header("Sayilar")]
     public int doorCount;
-    public GameObject GameManager;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            
+            GameManager.instance.playerLightCount -= doorCount;
+            gameObject.SetActive(false);
         }
     }
 
