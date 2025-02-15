b273fac32fb8ef950a7f13ae9dd52de6803101b7
Licaster0 <ahmet0434recep@gmail.com>
2025-02-15 13:35:52 +0300

    1234

--- MauJam2025/Assets/Scripts/Player/PlayerManager.cs
+++ MauJam2025/Assets/Scripts/Player/PlayerManager.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager instance;
 
+    [Header("Referances")]
     public Player player;
     public int playerLightCount;
+    public int Coin;
+    public TextMeshProUGUI coinText;
 
     private void Start()
     {
         playerLightCount = 1;
     }
-    public int Coin;
+    private void FixedUpdate()
+    {
+        UpdateCoinGui();
+    }
+    public void UpdateCoinGui()
+    {
+        coinText.text = Coin.ToString();
+    }
     void Awake()
     {
         if (instance == null)
