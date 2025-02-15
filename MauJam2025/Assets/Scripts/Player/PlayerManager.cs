ac4a8cd68993a52164d0cbf1c9aa7cdefc05390a
Licaster0 <ahmet0434recep@gmail.com>
2025-02-15 19:22:46 +0300

    12432

--- MauJam2025/Assets/Scripts/Player/PlayerManager.cs
+++ MauJam2025/Assets/Scripts/Player/PlayerManager.cs
@@ -1,39 +1,76 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager instance;
 
-    [Header("Referances")]
+    [Header("References")]
     public Player player;
     public int playerLightCount;
     public TextMeshProUGUI lightText;
+    public TextMeshProUGUI maxLightText;
     public int playerMaxLight;
     public int Coin;
     public TextMeshProUGUI coinText;
+
+    public Image[] lightImages; // UI'de kullandığınız ışıklar (Image array)
+    public Sprite fullLight; // Dolu ışık sprite'
+    public Sprite emptyLight; // Boş ışık sprite'
 
     private void Start()
     {
         playerLightCount = 1;
         playerMaxLight = playerLightCount;
+        UpdateGui(); // İlk GUI güncellemesi
     }
-    private void FixedUpdate()
+
+    private void Update()
     {
-        UpdateGui();
+        UpdateGui(); // UI her frame güncellenir
     }
-    public void MaxLightUpdate()
-    {
-        playerLightCount = playerMaxLight;
-    }
+
+    // GUI'yi güncelle
     public void UpdateGui()
     {
         coinText.text = Coin.ToString();
-        lightText.text = playerLightCount.ToString();
 
+        // İşığın durumunu UI'ye yansıt
+        for (int i = 0; i < lightImages.Length; i++)
+        {
+            // Işıklar arasında sadece gerekli olanları güncelle
+            if (i < playerMaxLight)
+            {
+                lightImages[i].gameObject.SetActive(true); // Işığı aktif et
+                lightImages[i].sprite = (i < playerLightCount) ? fullLight : emptyLight; // Dolu ya da boş
+            }
+            else
+            {
+                lightImages[i].gameObject.SetActive(false); // Gereksiz ışıkları gizle
+            }
+        }
+
+        // Light Text güncellemesi
+        lightText.text = playerLightCount.ToString() + "/" + playerMaxLight.ToString();
     }
+
+    // Işık sayısını arttırmak için fonksiyon
+    public void IncreaseMaxLight(int amount)
+    {
+        playerMaxLight = Mathf.Max(1, playerMaxLight + amount); // En az 1 ışık olmalı
+        MaxLightUpdate();
+    }
+
+    // Işık sayısını yenile
+    public void MaxLightUpdate()
+    {
+        playerLightCount = playerMaxLight;
+        playerLightCount = Mathf.Min(playerMaxLight, playerLightCount); // Mevcut ışık, max ışığı geçmesin
+    }
+
     void Awake()
     {
         if (instance == null)
@@ -42,7 +79,7 @@ public class PlayerManager : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // Singleton
         }
     }
 }
